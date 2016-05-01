b549d25a2dc58f81bbe6098395298bf613a97d7c
Ilia Khaliapin <962426@mail.ru>
2016-05-01 19:20:19 +0300

    Initial Commit

---
+++ VkPlaylist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VkPlaylist
+//
+//  Created by Илья Халяпин on 01.05.16.
+//  Copyright © 2016 Ilya Khalyapin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
