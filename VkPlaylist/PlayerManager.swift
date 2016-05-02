a550cf836f2370faacd60cd468db038621f4c42b
Ilia Khaliapin <962426@mail.ru>
2016-05-03 02:04:50 +0300

    При тапе по аудиозаписи, она запускается в фоне.

---
+++ VkPlaylist/PlayerManager.swift
@@ -0,0 +1,48 @@
+//
+//  PlayerManager.swift
+//  VkPlaylist
+//
+//  Created by Илья Халяпин on 03.05.16.
+//  Copyright © 2016 Ilya Khalyapin. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class PlayerManager {
+    
+    /* Паттерн Singleton */
+    
+    private struct Static {
+        static var onceToken: dispatch_once_t = 0 // Ключ идентифицирующий жизненынный цикл приложения
+        static var instance: PlayerManager? = nil
+    }
+    
+    class var sharedInstance : PlayerManager {
+        dispatch_once(&Static.onceToken) { // Для указанного токена выполняет блок кода только один раз за время жизни приложения
+            Static.instance = PlayerManager()
+        }
+        
+        return Static.instance!
+    }
+    
+    /* */
+    
+    
+    private init() {}
+    
+    
+    // MARK: Плеер
+    
+    var player: AVPlayer!
+    
+    // Старт аудиозаписи
+    func playFile(url: NSURL) {
+        if let player = player {
+            player.pause()
+        }
+        
+        player = AVPlayer(URL: url)
+        player.play()
+    }
+}
