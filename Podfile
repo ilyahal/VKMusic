13c63ca85deb092a0f58039c29dcd4bc6a65dab9
Ilia Khaliapin <962426@mail.ru>
2016-06-03 14:56:28 +0300

    Добавлен фреймворк AlamofireImage

--- Podfile
+++ Podfile
@@ -7,6 +7,7 @@ target 'VkPlaylist' do
 
 pod 'SwiftyVK', :git => 'https://github.com/WE-St0r/SwiftyVK.git'
 pod 'Alamofire', '~> 3.4'
+pod 'AlamofireImage', '~> 2.0'
 
 end
 
--- Pods/Target Support Files/Pods-VkPlaylist/Pods-VkPlaylist-frameworks.sh
+++ Pods/Target Support Files/Pods-VkPlaylist/Pods-VkPlaylist-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/AlamofireImage/AlamofireImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyVK/SwiftyVK.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/AlamofireImage/AlamofireImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyVK/SwiftyVK.framework"
 fi
