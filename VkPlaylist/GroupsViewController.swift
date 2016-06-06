de6b6786480a0e206f785b3b03c5f05efeb6d4c5
Ilia Khaliapin <962426@mail.ru>
2016-06-07 01:06:00 +0300

    Мини-плеер теперь также отображается на контроллерах со списком друзей и списком групп.

--- VkPlaylist/GroupsViewController.swift
+++ VkPlaylist/GroupsViewController.swift
@@ -11,4 +11,20 @@ import UIKit
 /// Контроллер содержащий контейнер со списком групп
 class GroupsViewController: UIViewController {
 
+    /// Контейнер в котором находится ViewController с мини-плеером
+    @IBOutlet weak var miniPlayerViewControllerContainer: UIView!
+    /// ViewController с мини-плеером
+    var miniPlayerViewController: MiniPlayerViewController {
+        return PlayerManager.sharedInstance.miniPlayerViewController
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        addChildViewController(miniPlayerViewController)
+        miniPlayerViewController.view.frame = CGRectMake(0, 0, miniPlayerViewControllerContainer.frame.size.width, miniPlayerViewControllerContainer.frame.size.height)
+        miniPlayerViewControllerContainer.addSubview(miniPlayerViewController.view)
+        miniPlayerViewController.didMoveToParentViewController(self)
+    }
+    
 }
